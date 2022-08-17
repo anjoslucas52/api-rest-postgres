f4838a8c981c42c39bbde785d2ce58c931a1fd37
Lucas Anjos <anjoslucas52@gmail.com>
2022-08-17 16:09:08 -0300

    Dependencias de projeto revisadas e start do Servidor

--- src/index.js
+++ src/index.js
@@ -1,10 +1,6 @@
 const express = require('express');
-const bodyParser = require('body-parser');
 
 const app = express();
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false}));
 
-
-app.listen(3001);
+app.listen(3333, () =>{console.log('Server Runing')});
