eab122481628185ee994566311c5e9da4f1ffa45
Lucas Anjos <anjoslucas52@gmail.com>
2022-08-18 09:55:47 -0300

    :wrench:Estabelecendo conexão com banco Postgres BANCOONE

---
+++ knexfile.js
@@ -0,0 +1,22 @@
+// Update with your config settings.
+
+/**
+ * @type { Object.<string, import("knex").Knex.Config> }
+ */
+module.exports = {
+
+  development: {
+    client: 'postgresql',
+    connection: {
+      database: 'bancoone',
+      host:'localhost',
+      port:5000,
+      user:     'root',
+      password: '9906'
+    },
+    migrations: {
+      tableName:'knex_migrations',
+      directory:`${__dirname}/src/database/migrations`
+    }
+  }
+};
